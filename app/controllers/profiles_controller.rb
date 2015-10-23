9e7c643840ae6ea5a85ffc38178941764bb3459e
Adam Debbagh <adamdebbagh@gmail.com>
2015-10-23 11:19:00 -0400

    added create and show actions

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,4 +3,25 @@ class ProfilesController < ApplicationController
     @user = User.find(params[:user_id]) # determines who's logged in
     @profile = @user.build_profile
   end
+  def show
+    
+  end
+
+  def create
+    @user = User.find(params[:user_id]) # geting this from the url
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
+      flash[:success] = 'Profile Updated'
+      redirect_to user_path(params[:user_id])
+    else
+      flash[:error] = 'Update Failed'
+      render action: :new
+    end
+
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:first_name,:last_name,:job_title,:phone_number,:contact_email,:description)
+  end
 end
